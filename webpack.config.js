31578635d89e0c67db01489037450ab5ef878b7b
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-03-14 11:35:22 +0100

    chore: init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Stylish = require('webpack-stylish')
+const path = require('path')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    bundle: './src/index.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts|.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      browser: 'google chrome',
+      port: 3000,
+      server: { baseDir: ['dist'] }
+    }),
+    new Stylish(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'Rail Fence Cipher'
+    })
+  ]
+}
